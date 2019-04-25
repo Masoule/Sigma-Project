94afbaa7acc04d0c6ac8b5f44acdc12dd2dfe82b
salome siavoshi <salomes@buildingiq.com>
2019-04-25 16:31:04 -0700

    auto generates nodes and edges

---
+++ graph.js
@@ -0,0 +1,56 @@
+var i,
+    s,
+    N = 100,
+    E = 500,
+    graph = {
+      nodes: [],
+      edges: []
+    };
+
+// Generate a random graph:
+for (i = 0; i < N; i++)
+  graph.nodes.push({
+    id: 'n' + i,
+    label: 'Node ' + i,
+    x: Math.random(),
+    y: Math.random(),
+    size: Math.random(),
+    color: '#666'
+  });
+
+for (i = 0; i < E; i++)
+  graph.edges.push({
+    id: 'e' + i,
+    source: 'n' + (Math.random() * N | 0),
+    target: 'n' + (Math.random() * N | 0),
+    size: Math.random(),
+    color: '#ccc',
+    // type: 'curvedArrow',
+  });
+
+  // Initialise sigma:
+s = new sigma({
+  graph: graph,
+  renderer: {
+      id: 'main-renderer',
+      container: document.getElementById('graph-container'),
+      // type: 'canvas',
+      type: 'svg',
+      // freeStyle: true
+  },
+  settings: {
+    enableHovering: false,
+    // edgeLabelSize: 'proportional',
+    // minArrowSize: 10
+  }
+});
+
+// load the graph
+// s.graph.read(graph);
+
+// draw the graph
+s.refresh();
+
+// launch force-atlas for 5sec
+// s.startForceAtlas2();
+// window.setTimeout(function() {s.killForceAtlas2()}, 10000);
